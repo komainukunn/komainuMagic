1891facd5902156cddbdb5ca0455324eb5867b09
koma <msy.register@gmail.com>
2015-07-02 00:32:14 +0900

    記事部分のreactの雛形のおおよそ完成

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,43 @@
+var React = require("react");
+var apiUrl = "https://komainukunn.herokuapp.com/api";
+apiUrl = "http://localhost:5000/api";
+
+var brogArticle = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    var markdownText = marked(this.state.data.text ? this.state.data.text : "");
+    if(this.state.data.categories){
+        var categoryComponent = this.state.data.categories.map(function(category){
+            return <a>#{category}</a>;
+        });
+    }
+    return (
+      <div className="commentBox">
+        <h2>{this.state.data.title}</h2>
+        <p>日付け : {this.state.data.date}</p>
+        <p>{categoryComponent}</p>
+        <div dangerouslySetInnerHTML={{__html: markdownText}}></div>
+      </div>
+    );
+  }
+});
+
+React.render(
+    React.createFactory(brogArticle)({url: apiUrl}), document.getElementById("brogContext")
+);
 
